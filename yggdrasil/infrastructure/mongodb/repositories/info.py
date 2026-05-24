ded7578584c5907624ddf2a90d27bd5ab0bbc11e
Vespera <me@vespera.moe>
2026-05-24 18:34:51 +0900

    feart: query exclude support

--- yggdrasil/infrastructure/mongodb/repositories/info.py
+++ yggdrasil/infrastructure/mongodb/repositories/info.py
@@ -46,9 +46,12 @@ class MongoDBInfoRepository(InfoRepository):
             infos.append(Info.from_dict(json_info))
         return infos
 
-    def _parse_query(self, querys: list[str]) -> tuple[str, dict[str, Any]]:
+    def _parse_query(
+        self, querys: list[str]
+    ) -> tuple[str, list[str], list[tuple[str, str, bool]]]:
         # Tags are received in the following format: female:big_breasts
         # If it is not in the following format, it is regarded as a title.
+        # Prefix with - to exclude: -female:big_breasts, -artist:name
         info_tags = {
             "artist": "artists",
             "group": "groups",
@@ -58,29 +61,37 @@ class MongoDBInfoRepository(InfoRepository):
             "character": "characters",
         }
         gender_common_tags = ["female", "tag", "male"]
-        query_dict: dict[str, Any] = {}
+        query_filters: list[tuple[str, str, bool]] = []
 
         title = ""
+        exclude_titles: list[str] = []
         for query in querys:
+            is_exclude = query.startswith("-")
+            if is_exclude:
+                query = query[1:]
+
             if query.startswith(tuple(map(lambda x: x + ":", info_tags.keys()))):
-                tag = query.split(":")
+                tag = query.split(":", 1)
                 key = tag[0]
                 value = tag[1]
-                if key in query_dict:
-                    query_dict[info_tags[key]]["$all"].append(value)
-                else:
-                    query_dict[info_tags[key]] = {"$all": [value]}
+                field = info_tags[key]
+                query_filters.append((field, value, is_exclude))
             elif query.startswith(tuple(map(lambda x: x + ":", gender_common_tags))):
-                if "tags" in query_dict:
-                    query_dict["tags"]["$all"].append(query)
-                else:
-                    query_dict["tags"] = {"$all": [query]}
+                query_filters.append(("tags", query, is_exclude))
             else:
-                title = query
-        return title, query_dict
+                if is_exclude:
+                    exclude_titles.append(query)
+                else:
+                    title = query
+        return title, exclude_titles, query_filters
 
     def make_pipeline(self, query: list[str]) -> list[dict[str, Any]]:
-        title, query_dict = self._parse_query(query)
+        title, exclude_titles, query_filters = self._parse_query(query)
+
+        query_dict: dict[str, Any] = {}
+        for field, value, is_exclude in query_filters:
+            op = "$nin" if is_exclude else "$all"
+            query_dict.setdefault(field, {}).setdefault(op, []).append(value)
 
         pipeline: list[dict[str, Any]] = [
             {"$project": {"_id": 0}},
@@ -104,6 +115,18 @@ class MongoDBInfoRepository(InfoRepository):
         else:
             pipeline[1]["$match"]["title"] = {"$regex": title, "$options": "i"}
 
+        sort_idx = next(i for i, s in enumerate(pipeline) if "$sort" in s)
+        for exclude_title in exclude_titles:
+            pipeline.insert(
+                sort_idx,
+                {
+                    "$match": {
+                        "title": {"$not": {"$regex": exclude_title, "$options": "i"}}
+                    }
+                },
+            )
+            sort_idx += 1
+
         return pipeline
 
     async def search_by_query(
@@ -118,11 +141,13 @@ class MongoDBInfoRepository(InfoRepository):
             ]
         )
 
+        count_pipeline = [
+            stage for stage in pipeline if "$match" in stage or "$search" in stage
+        ]
+        count_pipeline.append({"$count": "count"})
+
         count_cursor = await self.mongodb.collection.aggregate(
-            [
-                pipeline[0] if self.use_atlas_search else pipeline[1],
-                {"$count": "count"},
-            ],
+            count_pipeline,
             allowDiskUse=True,
         )
         result_cursor = await self.mongodb.collection.aggregate(
@@ -141,7 +166,7 @@ class MongoDBInfoRepository(InfoRepository):
 
     async def get_random_info(self, query: list[str]) -> Info | None:
         pipeline = self.make_pipeline(query)
-        del pipeline[2]  # remove sort
+        pipeline = [stage for stage in pipeline if "$sort" not in stage]
         pipeline.append({"$sample": {"size": 1}})
 
         async for json_info in await self.mongodb.collection.aggregate(
